d74417b6af4460a99d6f18329d0f2f5930922c11
“asifulhaque087” <asifulhaque087@gmail.com>
2023-07-04 17:47:22 +0600

    Add text field

--- src/components/roots/TextField.tsx
+++ src/components/roots/TextField.tsx
@@ -1,20 +1,27 @@
 'use client';
-
 import { useField, ErrorMessage } from 'formik';
 
 export const TextField = ({ label, ...props }: any) => {
   const [field, meta] = useField<{}>(props);
 
   return (
-    <div className="flex flex-wrap">
-      <div className="relative w-full appearance-none label-floating">
-        <input
-          className={`tracking-wide py-2 px-4 leading-relaxed appearance-none block w-full
-            rounded border border-gray-400  focus:outline-none focus:bg-white focus:border-2 focus:border-green-500  
-           ${meta.touched && meta.error && 'border border-red-500'}`}
-          {...field}
-          {...props}
-        />
+    <div>
+      <div className="my-3">
+        <label
+          htmlFor={label.replace(/ /g, '')}
+          className="py-2 block capitalize"
+        >
+          {label}
+        </label>
+        <div className="mb-3s pt-0">
+          <input
+            className={`px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white  rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full border ${
+              meta.touched && meta.error && ' border-red-500'
+            }`}
+            {...field}
+            {...props}
+          />
+        </div>
       </div>
       <ErrorMessage
         component="div"
