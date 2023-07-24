cc571d29edba51ce52ba3ab1be14322aaeef00a3
“asifulhaque087” <asifulhaque087@gmail.com>
2023-07-24 11:11:43 +0600

    Add pagination story

---
+++ src/stories/roots/Pagination.stories.tsx
@@ -0,0 +1,74 @@
+import React, { useMemo, useState } from 'react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { Pagination } from '@src/components/roots';
+import data from '@src/data.json';
+
+const meta: Meta<typeof Pagination> = {
+  title: 'Roots/Pagination',
+  component: Pagination,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Pagination>;
+
+let PageSize = 10;
+
+const PaginationWithHooks = () => {
+  // Sets the hooks for both the label and primary props
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  return (
+    <>
+      <div style={{ background: '#E5E5E5', padding: '10px' }}>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>FIRST NAME</th>
+              <th>LAST NAME</th>
+              <th>EMAIL</th>
+              <th>PHONE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentTableData.map((item, i) => {
+              return (
+                <tr key={i}>
+                  <td>{item.id}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <div style={{ marginTop: '100px' }}>
+          <Pagination
+            currentPage={currentPage}
+            totalCount={data.length}
+            pageSize={PageSize}
+            onPageChange={(page: any) => setCurrentPage(page)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const Primary: Story = {
+  render: () => <PaginationWithHooks />,
+};
