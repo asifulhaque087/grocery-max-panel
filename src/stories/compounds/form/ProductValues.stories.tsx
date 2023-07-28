092123e9aee2a28759ee31991c732df4f717f4fe
“asifulhaque087” <asifulhaque087@gmail.com>
2023-07-28 07:48:27 +0600

    Add story for product value

---
+++ src/stories/compounds/form/ProductValues.stories.tsx
@@ -0,0 +1,24 @@
+import { ProductValues } from '@src/components/compounds';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+
+const meta: Meta<typeof ProductValues> = {
+  title: 'Roots/Form/ProductValues',
+  component: ProductValues,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof ProductValues>;
+
+const Wrapper = (args: any) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return <ProductValues {...args} setActiveIndex={setActiveIndex} />;
+};
+
+export const Primary: Story = {
+  args: {},
+  render: (args) => <Wrapper {...args} />,
+};
