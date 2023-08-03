c5a03f8830c6d1a0dc1d293e8b703d5356bc8cd6
“asifulhaque087” <asifulhaque087@gmail.com>
2023-08-03 11:50:22 +0600

    Fromat product add page

--- src/app/(LayoutOne)/product/add/page.tsx
+++ src/app/(LayoutOne)/product/add/page.tsx
@@ -1,17 +1,8 @@
 'use client';
 
 import { ProductAddPage } from '@src/components/pages';
-import { useState } from 'react';
 
-import { Tab } from '@src/components/roots';
-import {
-  ProductAttribute,
-  ProductInformation,
-} from '@src/components/compounds';
-import client from '@src/graphql/client';
-import { gql, useQuery } from '@apollo/client';
-
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
 // async function fetchData() {
 //   const { loading, data: { attributes } = {} } = await client.query({
@@ -31,51 +22,7 @@ export const dynamic = 'force-dynamic';
 // const dataPromise = fetchData();
 
 const page = () => {
-  // return (
-  //   <div>
-  //     <ProductAddPage />
-  //   </div>
-  // );
-
-
-  const [activeTab, setActiveTab] = useState(0);
-
-  const tabs = [
-    {
-      title: 'informations',
-    },
-
-    {
-      title: 'variations',
-    },
-
-    {
-      title: 'combinations',
-    },
-  ];
-
-  return (
-    <div className="p-[20px]">
-      {/* tabs */}
-      <div>
-        <Tab
-          tabs={tabs}
-          activeIndex={activeTab}
-          setActiveIndex={setActiveTab}
-        />
-      </div>
-
-      {/* product information card */}
-      {activeTab == 0 ? (
-        <ProductInformation setActiveIndex={setActiveTab} />
-      ) : null}
-
-      {/* product attribute card */}
-      {activeTab == 1 ? (
-        <ProductAttribute setActiveIndex={setActiveTab} />
-      ) : null}
-    </div>
-  );
+  return <ProductAddPage fromEdit={false} />;
 };
 
 export default page;
