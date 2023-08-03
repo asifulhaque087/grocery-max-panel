d0987f2bf8cf6038cb264cb6f712520ce02c179a
“asifulhaque087” <asifulhaque087@gmail.com>
2023-08-03 11:50:04 +0600

    Fetch data prdouct edit page

--- src/app/(LayoutOne)/product/edit/[productId]/page.tsx
+++ src/app/(LayoutOne)/product/edit/[productId]/page.tsx
@@ -1,9 +1,36 @@
-import { ProductEditPage } from '@src/components/pages';
+'use client';
+export const dynamic = 'force-dynamic';
+
+import { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { ProductAddPage, ProductEditPage } from '@src/components/pages';
+import { FIND_CACHE_BY_PRODUCT_ID } from '@src/graphql/queries/productCacheQuery';
+import { storeSingleProduct } from '@src/graphql/reactivities/productVariable';
 
 const page = ({ params }: { params: { productId: string } }) => {
+  console.log('params is ', params.productId);
+
+  // fetching product by id
+  const { loading, data: { findCacheByProductId: product } = {} } = useQuery(
+    FIND_CACHE_BY_PRODUCT_ID,
+    {
+      variables: {
+        productId: parseInt(params.productId),
+      },
+    }
+  );
+
+  useEffect(() => {
+    // if (!product) storeSingleProduct(product);
+    // console.log("the product is ", product)
+    storeSingleProduct(product);
+  }, [product]);
+
   return (
     <div>
-      <ProductEditPage productId={params.productId} />
+      {/* <ProductEditPage productId={params.productId} /> */}
+      {/* this is product page */}
+      <ProductAddPage fromEdit={true} />
     </div>
   );
 };
