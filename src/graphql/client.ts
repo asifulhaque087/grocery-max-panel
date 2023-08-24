003feb8a39bc35c75ea971b7c2c7e7c46f1f7604
“asifulhaque087” <asifulhaque087@gmail.com>
2023-08-24 08:49:43 +0600

    Change server url

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -32,18 +32,18 @@
 
 // export default client;
 
-
-
-import { ApolloClient, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import cache from "./cache";
+import { ApolloClient, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import cache from './cache';
 
 let uri: any;
 
-if (process.env.NODE_ENV === "production") {
-  uri = "https://grocery-max-server.onrender.com/graphql";
+if (process.env.NODE_ENV === 'production') {
+  // uri = "https://grocery-max-server.onrender.com/graphql";
+  // uri = "https://grocery-max-server.onrender.com/graphql";
+  uri = 'http://grocery-max-server.asifulmridul.xyz/graphql';
 } else {
-  uri = "http://localhost:5000/graphql";
+  uri = 'http://localhost:5000/graphql';
 }
 
 const httpLink = createHttpLink({
@@ -51,10 +51,11 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext(() => {
-  const token = typeof window !== 'undefined' && sessionStorage.getItem("jwtToken") ;
+  const token =
+    typeof window !== 'undefined' && sessionStorage.getItem('jwtToken');
   return {
     headers: {
-      Authorization: token ? `Bearer ${token}` : "",
+      Authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
@@ -65,8 +66,6 @@ export const client = new ApolloClient({
 });
 
 export default client;
-
-
 
 // export const withApollo = createWithApollo(apolloClient);
 
