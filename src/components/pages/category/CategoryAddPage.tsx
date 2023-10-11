b87a36ed25832ed459be9b1a49b74b3c9f0925a2
“asifulhaque087” <asifulhaque087@gmail.com>
2023-10-11 12:59:00 +0600

    Update category add page

--- src/components/pages/category/CategoryAddPage.tsx
+++ src/components/pages/category/CategoryAddPage.tsx
@@ -1,9 +1,36 @@
 'use client';
 
-import { CategoryAddForm } from '@src/components/compounds';
+import { useQuery } from '@apollo/client';
+import { CategoryAddForm, TableLoading } from '@src/components/compounds';
+import { GET_CATEGORIES } from '@src/graphql/queries/categoryQuery';
+// import { categories } from '@src/stories/roots/form/SelectField.stories';
+import { formatItemsForReactSelect, organizeRecursiveItems } from '@src/utils';
 import { BiListUl } from 'react-icons/bi';
 
 export const CategoryAddPage = () => {
+  const {
+    loading: queryLoading,
+    data: { categories: categories } = {},
+    fetchMore,
+    // data,
+  } = useQuery(GET_CATEGORIES);
+
+  if (queryLoading) return <TableLoading />;
+
+  console.log('categoreis are ', categories);
+
+  const cats = formatItemsForReactSelect(
+    organizeRecursiveItems(categories, 'parentId', null),
+    'id',
+    'name'
+  );
+
+  console.log('the cats are ', cats);
+
+  const fetchCategories = () => {
+    fetchMore({});
+  };
+
   return (
     <div className="flex flex-col sm:flex-row items-start gap-[20px] p-[20px]  bg-[rgb(248,247,250)]">
       {/* top */}
@@ -24,7 +51,21 @@ export const CategoryAddPage = () => {
         </div>
 
         <hr className="border-t" />
-        <CategoryAddForm />
+        <CategoryAddForm cats={cats} fetchAgain={fetchCategories} />
+      </div>
+
+      {/* bottom */}
+
+      <div className="w-[40%]  rounded-[10px] bg-white shadow-custom">
+        <h4 className="text-[16px] font-[500] text-[#24334A] capitalize py-[12px] px-[25px] border-b">
+          Category overview
+        </h4>
+
+        <div className="p-[24px]">
+          {cats.map((cat) => (
+            <p key={cat.label}>{cat.label}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
