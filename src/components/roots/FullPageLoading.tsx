cc83f21eaf04e478452393049be7ea12787213ff
“asifulhaque087” <asifulhaque087@gmail.com>
2023-10-13 19:26:31 +0600

    Update loading

--- src/components/roots/FullPageLoading.tsx
+++ src/components/roots/FullPageLoading.tsx
@@ -2,10 +2,8 @@
 
 export const FullPageLoading = () => {
   return (
-    <div>
-      <div className="h-screen w-full flex items-center justify-center">
-        <div className="h-7 w-7 rounded-full border-dotted border-2  border-gray-500 animate-spin ease-linear mr-3"></div>
-      </div>
+    <div className="h-screen w-full grid place-items-center">
+      <div id="spinner" />
     </div>
   );
 };
