59e9f07f110e5847ae8b4237cb9276ffb164a067
“asifulhaque087” <asifulhaque087@gmail.com>
2023-10-16 10:31:56 +0600

    Add remove attribute mutation

--- src/graphql/mutations/AttributeMutation.ts
+++ src/graphql/mutations/AttributeMutation.ts
@@ -1,19 +1,19 @@
-
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 // ============================= CREATE CATEGORY MUTATION =================>
 export const CREATE_ATTRIBUTE = gql`
-  mutation createBanner($photo: String!) {
-    createBanner(input: { photo: $photo }) {
-      errors {
-        field
-        message
-      }
-      banner {
-        id
-        photo
-        createdAt
-      }
+  mutation createAttribute($name: String!) {
+    createAttribute(createAttributeInput: { name: $name }) {
+      id
+      name
+    }
+  }
+`;
+
+export const REMOVE_ATTRIBUTE = gql`
+  mutation removeAttribute($id: Int!) {
+    removeAttribute(id: $id) {
+      id
     }
   }
 `;
