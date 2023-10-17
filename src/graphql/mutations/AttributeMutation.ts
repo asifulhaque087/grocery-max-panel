53b1e769229db563364d55a4c994fa3ac6bc58e2
“asifulhaque087” <asifulhaque087@gmail.com>
2023-10-17 11:24:26 +0600

    Add update atribute

--- src/graphql/mutations/AttributeMutation.ts
+++ src/graphql/mutations/AttributeMutation.ts
@@ -10,6 +10,15 @@ export const CREATE_ATTRIBUTE = gql`
   }
 `;
 
+export const UPDATE_ATTRIBUTE = gql`
+  mutation updateAttribute($id: Int!, $name: String!) {
+    updateAttribute(updateAttributeInput: { id: $id, name: $name }) {
+      id
+      name
+    }
+  }
+`;
+
 export const REMOVE_ATTRIBUTE = gql`
   mutation removeAttribute($id: Int!) {
     removeAttribute(id: $id) {
